c61132a5ac0d757fcfa8e44ed510abc776a25b2d
heckerhai <darkysenpaii@gmail.com>
2025-04-08 20:48:39 +0530

    program to implements inheritance.

--- Q13.php
+++ Q13.php
@@ -21,3 +21,66 @@ $pet = new Dog();
 $pet->sound();  // from Animal
 $pet->bark();   // from Dog
 ?>
+
+//Multi-level inheritance
+<?php
+// Base class
+class Grandfather {
+    public function showGrandfather() {
+        echo "I am the Grandfather.<br>";
+    }
+}
+
+// First level child
+class Father extends Grandfather {
+    public function showFather() {
+        echo "I am the Father.<br>";
+    }
+}
+
+// Second level child (inherits from Father)
+class Son extends Father {
+    public function showSon() {
+        echo "I am the Son.<br>";
+    }
+}
+
+// Create object of the lowest class
+$person = new Son();
+$person->showGrandfather();
+$person->showFather();
+$person->showSon();
+?>
+
+//Multiple inheritance (via traits)
+<?php
+// Trait 1
+trait Engine {
+    public function startEngine() {
+        echo "Engine started.<br>";
+    }
+}
+
+// Trait 2
+trait MusicSystem {
+    public function playMusic() {
+        echo "Playing music...<br>";
+    }
+}
+
+// Class using both traits
+class Car {
+    use Engine, MusicSystem;
+
+    public function drive() {
+        echo "Car is driving.<br>";
+    }
+}
+
+// Create object of Car
+$myCar = new Car();
+$myCar->startEngine();
+$myCar->playMusic();
+$myCar->drive();
+?>
+
