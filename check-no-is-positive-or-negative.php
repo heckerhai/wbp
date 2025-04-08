135f6340bd31fea1914c883054bf4f15d47440ac
heckerhai <darkysenpaii@gmail.com>
2025-04-08 19:54:05 +0530

    Create check-no-is-positive-or-negative.php

---
+++ check-no-is-positive-or-negative.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Check Positive or Negative</title>
+</head>
+<body>
+
+<form method="post">
+  Enter a number: <input type="number" name="num"><br><br>
+  <input type="submit" name="check" value="Check">
+</form>
+
+<?php
+if (isset($_POST['check'])) {
+  $num = $_POST['num'];
+
+  if ($num > 0) {
+    echo "The number is Positive";
+  } elseif ($num < 0) {
+    echo "The number is Negative";
+  } else {
+    echo "The number is Zero";
+  }
+}
+?>
+
+</body>
+</html>
