c58461214eb45666ffab68567cdac0223ec7e7c0
heckerhai <darkysenpaii@gmail.com>
2025-04-08 22:19:20 +0530

    Write a calendar program using switch statement.

--- Q4.php
+++ Q4.php
@@ -1,3 +1,51 @@
+<?php
+// Initialized month number
+$month = 4;
+
+switch ($month) {
+    case 1:
+        echo "January";
+        break;
+    case 2:
+        echo "February";
+        break;
+    case 3:
+        echo "March";
+        break;
+    case 4:
+        echo "April";
+        break;
+    case 5:
+        echo "May";
+        break;
+    case 6:
+        echo "June";
+        break;
+    case 7:
+        echo "July";
+        break;
+    case 8:
+        echo "August";
+        break;
+    case 9:
+        echo "September";
+        break;
+    case 10:
+        echo "October";
+        break;
+    case 11:
+        echo "November";
+        break;
+    case 12:
+        echo "December";
+        break;
+    default:
+        echo "Invalid month number.";
+}
+?>
+
+
+//⚠️Just in case user input is required
 <!DOCTYPE html>
 <html>
 <head>
