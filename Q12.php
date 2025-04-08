82f37429f906a571b676c7e0cb663641a550cfd0
heckerhai <darkysenpaii@gmail.com>
2025-04-08 20:28:06 +0530

    program to demonstrate Variable function.

---
+++ Q12.php
@@ -0,0 +1,14 @@
+<?php
+// Define a function
+function carBrand($name) {
+    echo "Car brand is $name.<br>";
+}
+
+// Store the function name in a variable
+$myFunc = "carBrand";
+
+// Call the function using the variable
+$myFunc("Toyota");
+$myFunc("BMW");
+$myFunc("Hyundai");
+?>
