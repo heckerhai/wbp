f887dcf49f4a617188917165de724b43e1da8aa8
heckerhai <darkysenpaii@gmail.com>
2025-04-08 19:51:25 +0530

    Update maketheuseoflogicaloperators.php

--- maketheuseoflogicaloperators.php
+++ maketheuseoflogicaloperators.php
@@ -1,1 +1,17 @@
+<?php
+$a = 10;
+$b = 20;
+
+if ($a < 15 && $b > 15) {
+    echo "AND: Both true<br>";
+}
+
+if ($a > 15 || $b > 15) {
+    echo "OR: One is true<br>";
+}
+
+if (!($a > 15)) {
+    echo "NOT: Condition is false<br>";
+}
+?>
 
