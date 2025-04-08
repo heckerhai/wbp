d6f2a4ece4e648b649285cf091016fada6a12c6a
heckerhai <darkysenpaii@gmail.com>
2025-04-08 20:23:34 +0530

    program to demonstrate parameterized function

---
+++ Q10.php
@@ -0,0 +1,12 @@
+<?php
+// Function that takes car brand as parameter
+function showCarBrand($brand) {
+    echo "This car is a $brand.<br>";
+}
+
+// Calling the function with different car brands
+showCarBrand("Toyota");
+showCarBrand("BMW");
+showCarBrand("Honda");
+showCarBrand("Tesla");
+?>
