90573fc861922d5fab56ccf18bd3e9880ec436ae
heckerhai <darkysenpaii@gmail.com>
2025-04-08 19:59:20 +0530

    Create calendar-program-using-switch-statement.php

---
+++ calendar-program-using-switch-statement.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Calendar Program</title>
+</head>
+<body>
+
+<form method="post">
+  Enter month number (1-12): <input type="number" name="month" min="1" max="12"><br><br>
+  <input type="submit" name="show" value="Show Month Details">
+</form>
+
+<?php
+if (isset($_POST['show'])) {
+  $month = $_POST['month'];
+
+  switch ($month) {
+    case 1:
+      echo "January ";
+      break;
+    case 2:
+      echo "February";
+      break;
+    case 3:
+      echo "March";
+      break;
+    case 4:
+      echo "April";
+      break;
+    case 5:
+      echo "May";
+      break;
+    case 6:
+      echo "June";
+      break;
+    case 7:
+      echo "July";
+      break;
+    case 8:
+      echo "August";
+      break;
+    case 9:
+      echo "September";
+      break;
+    case 10:
+      echo "October";
+      break;
+    case 11:
+      echo "November";
+      break;
+    case 12:
+      echo "December";
+      break;
+    default:
+      echo "Invalid month number. Please enter a number between 1 and 12.";
+  }
+}
+?>
+
+</body>
+</html>
