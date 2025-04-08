507ab5631a576324cf67bfaf50dd5de5c44a66c5
heckerhai <darkysenpaii@gmail.com>
2025-04-08 22:15:59 +0530

    calculate Highest number among three numbers.

--- Q1.php
+++ Q1.php
@@ -1,34 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Highest Number</title>
-</head>
-<body>
-
-<form method="post">
-  Enter first number: <input type="number" name="num1"><br><br>
-  Enter second number: <input type="number" name="num2"><br><br>
-  Enter third number: <input type="number" name="num3"><br><br>
-  <input type="submit" name="submit" value="Find Highest">
-</form>
-
-  
 <?php
-if (isset($_POST['submit'])) {
-  $a = $_POST['num1'];
-  $b = $_POST['num2'];
-  $c = $_POST['num3'];
+// Already initialized values
+$a = 20;
+$b = 35;
+$c = 15;
 
-  if ($a >= $b && $a >= $c) {
+// Find highest number
+if ($a >= $b && $a >= $c) {
     echo "The highest number is: $a";
-  } elseif ($b >= $a && $b >= $c) {
+} elseif ($b >= $a && $b >= $c) {
     echo "The highest number is: $b";
-  } else {
+} else {
     echo "The highest number is: $c";
-  }
 }
 ?>
-
-</body>
-</html>
-
