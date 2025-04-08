be996b7f201a0831301f963f0c8f5a739c6e1726
heckerhai <darkysenpaii@gmail.com>
2025-04-08 20:21:41 +0530

    a program to using Indexed array.

---
+++ Q9.php
@@ -0,0 +1,9 @@
+<?php
+// Indexed array of fruits
+$fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+// Display the fruits using a loop
+foreach ($fruits as $fruit) {
+    echo "$fruit<br>";
+}
+?>
