f52e34810e0fd12737b945cd6e646e25cce3e7af
heckerhai <darkysenpaii@gmail.com>
2025-04-23 18:53:17 +0530

    display pyramids of star/patterns using increment/  decrement.

--- Q6.php
+++ Q6.php
@@ -1,37 +1,23 @@
 <?php
+
+for ($i = 1; $i <= 5; $i++) {
+    echo str_repeat("*", $i) . "<br>";
+}
+
 $rows = 5;
 
 for ($i = 1; $i <= $rows; $i++) {
-    // First triangle (right aligned, no space between stars)
-    for ($j = 0; $j < $rows - $i; $j++) {
-        echo " ";
-    }
-    for ($k = 0; $k < $i; $k++) {
-        echo "*";
-    }
-
-    // Space between first and second triangle
-    echo "     ";
-
-    // Second triangle (right aligned, stars with space)
-    for ($j = 1; $j <= $rows - $i; $j++) {
-        echo " ";
-    }
-    for ($k = 1; $k <= $i; $k++) {
-        echo "* ";
-    }
-
-    // Space between second and third triangle
-    echo "    ";
-
-    // Third triangle (no space, same as first one)
-    for ($j = 1; $j <= $rows - $i; $j++) {
-        echo " ";
-    }
-    for ($k = 1; $k <= $i; $k++) {
-        echo "*";
-    }
-
-    echo "\n";
+    echo str_repeat("&nbsp;", $rows - $i);
+    echo str_repeat("*", $i);
+    echo "<br>";
 }
+
+$rows = 5;
+
+for ($i = 1; $i <= $rows; $i++) {
+    echo str_repeat("&nbsp;&nbsp;", $rows - $i);
+    echo str_repeat("*", $i);
+    echo "<br>";
+}
+
 ?>
