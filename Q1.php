b776d02e64a3f96c5244c1808c050fc4bce97237
heckerhai <darkysenpaii@gmail.com>
2025-04-23 18:40:24 +0530

    Write a PHP program to calculate Highest number among three numbers.

--- Q1.php
+++ Q1.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        Enter a first number : <input type="number" name="a" required><br><br>
+        Enter a second number : <input type="number" name="b" required><br><br>
+        Enter a third number : <input type="number" name="c" required><br><br>
+        <input type="submit" value="Check Greatest">
+    </form>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $a = $_POST['a'];
+        $b = $_POST['b'];
+        $c = $_POST['c'];
+        if ($a >= $b && $a >= $c) {
+            echo $a . ' is greatest';
+        } elseif ($b >= $a && $b >= $c) {
+            echo $b . ' is greatest';
+        } else {
+            echo $c . ' is greatest';
+        }
+    }
+    ?>
+</body>
+</html>
+
+___________________________________________________________________________________________________________________________
+
+
 <?php
 // Already initialized values
 $a = 20;
